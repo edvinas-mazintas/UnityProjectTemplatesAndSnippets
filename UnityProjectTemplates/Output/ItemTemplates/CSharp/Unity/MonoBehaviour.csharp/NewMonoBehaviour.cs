daefbe466cf28c1f776b2ceecadda7f1c9c8fc9f
Edvinas <kimold2323@gmail.com>
2023-07-11 13:48:49 +0300

    Update stuff

---
+++ UnityProjectTemplates/Output/ItemTemplates/CSharp/Unity/MonoBehaviour.csharp/NewMonoBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : MonoBehaviour
+    {
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
